0157edbb06c58b0db7c0d9ab4fbb36438d31ac1f
Ilia A <lawudetsey@gmail.com>
2026-03-14 16:25:07 -0400

    added survey-form component

---
+++ src/app/survey-form/survey-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-form',
+  imports: [],
+  templateUrl: './survey-form.html',
+  styleUrl: './survey-form.css',
+})
+export class SurveyForm {}
