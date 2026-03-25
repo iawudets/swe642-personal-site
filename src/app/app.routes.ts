42478417af8abd5a45a7c880b840da5c694f2b3e
Ilia A <lawudetsey@gmail.com>
2026-03-25 14:37:31 -0400

    added routing for survey form and list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SurveyForm } from './survey-form/survey-form';
+import { SurveyList } from './survey-list/survey-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dept-survey',
+    component: SurveyForm
+  },
+  {
+    path: 'survey-list',
+    component: SurveyList
+  }
+];
---
+++ src/app/survey-list/survey-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-list',
+  imports: [],
+  templateUrl: './survey-list.html',
+  styleUrl: './survey-list.css',
+})
+export class SurveyList {}
