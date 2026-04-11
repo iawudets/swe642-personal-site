f084c7c526a510bee875c8f80df0e5d396689493
Ilia A <lawudetsey@gmail.com>
2026-04-11 13:58:11 -0400

    survey entity model complete

--- backend/src/main/java/com/swe642/entities/Survey.java
+++ backend/src/main/java/com/swe642/entities/Survey.java
@@ -1,29 +1,102 @@
 package com.swe642.entities;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Date;
 
 @Repository
 @Table(name = "SURVEY")
 public class Survey implements java.io.Serializable {
 
+  private enum UniversityInterestChoice {
+    FRIENDS,
+    TELEVISION,
+    INTERNET,
+    OTHER
+  }
+  private enum CampusLikeChoice {
+    STUDENTS,
+    LOCATION,
+    CAMPUS,
+    ATMOSPHERE,
+    DORM,
+    SPORTS
+  }
+  private enum RecommendLikelihoodChoice {
+    VERY_LIKELY,
+    LIKELY,
+    UNLIKELY
+  }
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "SURVEY_ID", nullable=false)
   private long surveyId;
+
+  @Column(name = "TIME_CREATED", nullable = false)
+  private long timeCreated;
+
+  @Column(name = "SURVEY_DATE" , nullable=false)
   private Date surveyDate;
+
+  @Column(name = "FIRST_NAME" , nullable=false)
   private String firstName;
+
+  @Column(name = "LAST_NAME" , nullable=false)
   private String lastName;
+
+  @Column(name = "STREET" , nullable=false)
   private String street;
 
-  public Survey() { }
+  @Column(name = "ZIP" , nullable=false)
+  private String zip;
 
+  @Column(name = "CITY" , nullable=false)
+  private String city;
 
+  @Column(name = "STATE" , nullable=false)
+  private String state;
 
+  @Column(name = "PHONE_NUMBER" , nullable=false)
+  private String phoneNumber;
+
+  @Column(name = "EMAIL" , nullable=false)
+  private String email;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "CAMPUS_LIKE_CHOICE" , nullable=false)
+  private CampusLikeChoice campusLikeChoice;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "UNIVERSITY_INTEREST_CHOICE")
+  private UniversityInterestChoice universityInterestChoice;
+
+  @Column(name = "GRAD_MONTH" , nullable=false)
+  private String gradMonth;
+
+  @Column(name = "GRAD_YEAR" , nullable=false)
+  private String gradYear;
+
+  @Column(name = "COMMENT" , nullable=false)
+  private String comment;
+
+  public Survey(Date surveyDate, String firstName, String lastName, String street, String zip, String city, String state, String phoneNumber, String email, CampusLikeChoice campusLikeChoice, UniversityInterestChoice universityInterestChoice, String gradMonth, String gradYear, String comment) {
+    this.timeCreated = Instant.now().getEpochSecond();
+    this.surveyDate = surveyDate;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.street = street;
+    this.zip = zip;
+    this.city = city;
+    this.state = state;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.campusLikeChoice = campusLikeChoice;
+    this.universityInterestChoice = universityInterestChoice;
+    this.gradMonth = gradMonth;
+    this.gradYear = gradYear;
+    this.comment = comment;
+  }
 }
