f2df6eb02dcc1ec8e97cc8c76541a17d64f5b76d
Ilia Awudetsey <lawudetsey@gmail.com>
2026-04-27 00:26:10 -0400

    updated survey view refs and zoning

--- frontend/src/app/survey-view/survey-view.ts
+++ frontend/src/app/survey-view/survey-view.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import {ActivatedRoute, RouterLink, RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-survey-view',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './survey-view.html',
   styleUrl: './survey-view.css',
 })
 export class SurveyView implements OnInit {
   survey_id : string | null = null;
   survey_data : any | null = null;
-  survey_json : any | null = null;
+  survey : any | null = null;
 
   url_string : string = "http://localhost:8080/surveys/"
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private cdr : ChangeDetectorRef) {
+    this.route = route;
+    this.cdr = cdr;
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -27,6 +35,7 @@ export class SurveyView implements OnInit {
     this.survey_data = await fetch(this.url_string + this.survey_id, {
       method: "GET"
     });
-    this.survey_json = await this.survey_data.json();
+    this.survey = await this.survey_data.json();
+    this.cdr.markForCheck();
   }
 }
